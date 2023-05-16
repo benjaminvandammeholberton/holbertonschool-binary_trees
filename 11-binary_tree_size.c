932a576498acdb3f682e5e760a04a5ac26236c7a
Ben <holberton@pl118833.ag1313.nttpc.ne.jp>
2023-05-16 10:35:34 +0200

    Add: 10-binary_tree_depth.c 11-binary_tree_size.c 12-binary_tree_leaves.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: the size of the binary tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+        size_t right, left;
+
+	if (tree == NULL)
+                return (0);
+       
+	right = binary_tree_size(tree->right);
+	left = binary_tree_size(tree->left);
+
+	return (left + right + 1);	
+}
+
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: the number of leaves in the tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+        size_t right, left;
+
+	if (tree == NULL)
+                return (0);
+
+        if (!tree->right && !tree->left)
+        	return (1);
+       
+	right = binary_tree_leaves(tree->right);
+	left = binary_tree_leaves(tree->left);
+
+	return (left + right);
+}
+
