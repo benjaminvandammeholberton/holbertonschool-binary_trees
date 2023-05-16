4e232be9b37190e6b52ba7be2658f4dbdaf1a6dc
root <6281@holbertonstudents.com>
2023-05-16 08:39:43 -0700

    Fix: change the malloc function by the function binary_tree_node in the file 1 insert node left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function that inserts a node as the left-child
- * of another node
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
- * Return: pointer to the new node, NULL on failure or if parent is NULL
+ * binary_tree_insert_left - Inserts a node as the left-child of another node.
+ * @parent: Pointer to the node to insert the left-child in.
+ * @value: Value to store in the new node.
+ *
+ * Return: A pointer to the created node, or NULL on failure or if parent is NULL.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -19,15 +19,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->parent = parent;
-	new_node->n = value;
-
-	if (parent->left == NULL)
+	if (parent->left != NULL)
 	{
-		parent->left = new_node;
-		return (parent);
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
-	new_node->left = parent->left;
+
 	parent->left = new_node;
+
 	return (new_node);
 }
