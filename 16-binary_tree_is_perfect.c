6ddc317778b3221c8744121d115f2916d7ca6a42
Ben <holberton@pl118833.ag1313.nttpc.ne.jp>
2023-05-16 18:40:27 +0200

    Fix: function "16-binary_tree_is_perfect", insert function "binary_tree_leaves"

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,6 +23,29 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: the number of leaves in the tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+        size_t right, left;
+
+	if (tree == NULL)
+                return (0);
+
+        if (!tree->right && !tree->left)
+        	return (1);
+       
+	right = binary_tree_leaves(tree->right);
+	left = binary_tree_leaves(tree->left);
+
+	return (left + right);
+}
+
+
+/**
  * binary_tree_is_perfect - Check if a binary tree is perfect.
  * @tree: A pointer to the root node of the tree to check.
  *
@@ -32,27 +55,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if ((binary_tree_is_full(tree) == 1)
-	    && (binary_tree_height(tree->left)
-		== binary_tree_height(tree->right)))
+	if ((binary_tree_leaves(tree->left) == binary_tree_leaves(tree->right)) && (binary_tree_height(tree->left) == binary_tree_height(tree->right)))
 		return (1);
 	return (0);
 }
 
-/**
- * binary_tree_is_full - function that checks if a binary tree is full
- * @tree: pointer
- * Return: balance count difference
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-	if (binary_tree_is_full(tree->left) == 1
-	   && binary_tree_is_full(tree->right) == 1)
-		return (1);
-	else
-		return (0);
-}
+#include "binary_trees.h"
+
+
