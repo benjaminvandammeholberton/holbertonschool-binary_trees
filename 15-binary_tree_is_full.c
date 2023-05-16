8380d0f7322ce2f6c6808200d11cba945493654b
root <6281@holbertonstudents.com>
2023-05-16 05:14:02 -0700

    Fix: correct the syntax and the condition if the tree is NULL in the file 15 tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,12 +6,15 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return(1);
+		return (1);
 	if (tree->left == NULL || tree->right == NULL)
-		return(0);
-	if(binary_tree_is_full(tree->left) == 1 && binary_tree_is_full(tree->right) == 1)
-		return(1);
+		return (0);
+	if (binary_tree_is_full(tree->left) == 1
+	    && binary_tree_is_full(tree->right) == 1)
+		return (1);
 	else
-		return(0);
+		return (0);
 }
