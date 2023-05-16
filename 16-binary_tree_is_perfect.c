ec17b773810c9f5827f50f05c10a3e89f89f6fc1
benjaminvandammeholberton <benjamin.vandamme@me.com>
2023-05-16 22:28:44 +0200

    Fix: in "16-binary_tree_is_perfect.c", replace the function that counts leaves

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,14 +30,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        size_t right, left;
+	size_t right, left;
 
 	if (tree == NULL)
-                return (0);
+		return (0);
 
-        if (!tree->right && !tree->left)
-        	return (1);
-       
+	if (!tree->right && !tree->left)
+		return (1);
+
 	right = binary_tree_leaves(tree->right);
 	left = binary_tree_leaves(tree->left);
 
@@ -55,11 +55,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if ((binary_tree_leaves(tree->left) == binary_tree_leaves(tree->right)) && (binary_tree_height(tree->left) == binary_tree_height(tree->right)))
+	if ((binary_tree_leaves(tree->left) == binary_tree_leaves(tree->right))
+	    && (binary_tree_height(tree->left)
+		== binary_tree_height(tree->right)))
 		return (1);
 	return (0);
 }
-
-#include "binary_trees.h"
-
-
