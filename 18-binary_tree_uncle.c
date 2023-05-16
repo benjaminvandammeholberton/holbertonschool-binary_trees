fac95fa6a508a1434434386fd8f3a5dd4ddcef59
Ben <holberton@pl118833.ag1313.nttpc.ne.jp>
2023-05-16 16:32:01 +0200

    Add: documentation for function "18-binary_tree_uncle.c"

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -17,6 +17,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	return (binary_tree_sibling(node->parent));
 }
 
+/**
+ * binary_tree_sibling -  function that finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: pointer to the sibling node
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node ==  NULL)
