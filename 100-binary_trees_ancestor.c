2bd91b306fb036111d41f473c1f48e2f91d41421
root <6281@holbertonstudents.com>
2023-05-17 02:29:01 -0700

    Fix: change thefunction on interative and not on recursive, on the file 100 to check the ancestor common

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,23 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - function that finds the lowest common ancestor of
- * two nodes
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: pointer to the lowest common ancestor node of the two given nodes
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ *
+ * Return: Pointer to the lowest common ancestor node.
+ *         If no common ancestor was found, returns NULL.
  */
-
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	const binary_tree_t *node1 = first;
+	const binary_tree_t *node2 = second;
+
 	if (first == NULL || second == NULL)
 		return (NULL);
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
-	if (second->parent == first)
-		return ((binary_tree_t *)first);
-	if (first->parent == second->parent)
-		return (first->parent);
-	return (binary_trees_ancestor(first->parent, second->parent));
+
+	while (node1 != node2)
+	{
+		if (node1 == NULL)
+			node1 = second;
+		else
+			node1 = node1->parent;
+
+		if (node2 == NULL)
+			node2 = first;
+		else
+			node2 = node2->parent;
+	}
+
+	return (binary_tree_t *)node1;
 }
